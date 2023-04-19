fe6ef13dee8589760a13e34711d699607c75f04f
Carlos Perez <carlospe.mateos@gmail.com>
2023-04-19 13:41:25 +0200

    Add: Insert module

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from model.user_connection import UserConnection
+from schema.user_schema import UserSchema
 
 app = FastAPI()
+conn = UserConnection()
 
 @app.get("/")
 async def root():
+    conn
     return {"message": "Hello World"}
+
+
+@app.post("/api/insert")
+async def insert(user_data: UserSchema):
+    data = user_data.dict()
+    data.pop("id")
+    conn.write(data)
