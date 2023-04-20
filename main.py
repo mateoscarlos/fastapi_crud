a199a35e398de2d3c703b1a7842098bdc491e17b
Carlos Perez <carlospe.mateos@gmail.com>
2023-04-20 20:33:47 +0200

    Add: Read single data

--- main.py
+++ main.py
@@ -7,7 +7,25 @@ conn = UserConnection()
 
 @app.get("/")
 async def root():
-    return conn.read_all()
+    items = []
+    for data in conn.read_all():
+        dictionary = {}
+        dictionary["id"] = data[0]
+        dictionary["name"] = data[1]
+        dictionary["phone"] = data[2]
+        items.append(dictionary)
+
+    return items
+
+@app.get("/api/user/{id}")
+async def get_one(id: str):
+    dictionary = {}
+    data = conn.read(id)
+    dictionary["id"] = data[0]
+    dictionary["name"] = data[1]
+    dictionary["phone"] = data[2]
+
+    return dictionary
 
 @app.post("/api/insert")
 async def insert(user_data: UserSchema):
